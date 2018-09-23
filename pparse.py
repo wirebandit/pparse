72b3b7d30ee176e7c378c79b1e562ba57dea69e4
Matt Rosenberg <wirebandit@protonmail.com>
2018-09-23 19:09:29 -0400

    File input finished. Version 0.1 -> Version 0.2

--- pparse.py
+++ pparse.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
-'''pparse will accept file,text blob, or url string as input. pparse will translate found ProofPoint URL's, replace them, and output to file or stdout
-'''
+"""
+pparse will accept file,text blob, or url string as input. pparse will translate found ProofPoint URL's, replace them, and output to file or stdout
+"""
 __author__ = ["Matt Rosenberg"]
 __credits__ = ["Matt Rosenberg"]
 __license__ = "GNU GPL v3"
-__version__ = "0.1"
+__version__ = "0.2"
 __maintainer__ = ["Matt Rosenberg"]
 __email__ = ["wirebandit@protonmail.com"]
 __status__ = "Development"
@@ -14,84 +15,124 @@ import argparse
 import logging
 import re
 from urllib.parse import parse_qs, unquote, urlparse
+import sys
 
+# Global Vars
+regex = re.compile('https://urldefense\.proofpoint\.com/v\d/url\?\S+=', re.IGNORECASE)
 
-## SECTION - Logging
-
-# Set logging level
-if debug:
-    debug_level = 'DEBUG'
-else:
-    debug_level = 'INFO'
-
-# Create basic logging
-logging.basicConfig(
-        level=debug_level
-        filename=debug_file,
-        filemode='a',
-        format='%(asctime)s:%(levelname)s:%(message)s')
 
 ## SECTION - Functions
 
 def translate_url(data):
     try:
-        logging.DEBUG('Decoding URL..')
-        decoded = unquote(parse_qs(urlparse(url).query)['u'][0].translate(str.maketrans('-_', '%/')))
-        logging.INFO('Decoded URL: {}'.format(decoded))
+        logging.debug('Decoding URL..')
+        decoded = unquote(parse_qs(urlparse(data).query)['u'][0].translate(str.maketrans('-_', '%/')))
+        logging.info('Decoded URL: {}'.format(decoded))
         return decoded
     except Exception as e:
-        logging.CRITICAL('Error while parsing URL: {}'.format(e))
-
+        logging.error('Error while parsing URL: {}'.format(e))
+        print('Error: {}'.format(e))
 
 
 def parse_text(data):
-    logging.DEBUG('URL Found in string. Making attempt to decode.')
-    regex = re.compile('https://urldefense\.proofpoint\.com/v\d/url\?\S+=', re.IGNORECASE)
+    logging.debug('URL Found in string. Making attempt to decode.')
     cleaned_string = re.sub(regex, lambda m: translate_url(m.group()), data)
     return cleaned_string
 
 
-def findall_url(data):
-    regex = re.compile('https://urldefense\.proofpoint\.com/v\d/url\?\S+=', re.IGNORECASE)
+def findall_url_write(data, fh):
     while True:
         if re.search(regex, data):
             data = parse_text(data)
             continue
         else:
-            break
-    return data
+            try:
+                fh.write(data)
+                break
+            except IOError:
+                logging.error('Error: {}'.format(e))
+                print('Error: {}'.format(e))
 
 
-def write_out():
-    #TODO add function to write to file
-
+# def write_out():
+# TODO add function to write to file
 
 def main(arguments):
-
-    # Setup arguments
     parser = argparse.ArgumentParser()
     parser.add_argument('-f', '--file', dest='in_file', help='File to parse')
     parser.add_argument('-b', '--blob', dest='in_blob', help='Accepts a blob of text surround in quotes')
     parser.add_argument('-u', '--url', dest='in_url', help='Url to parse')
     parser.add_argument('-o', '--outfile', dest='out_file', help='Filename of output file')
-    parser.add_argument('-O', '--overwrite', dest='overwrite', default=False, action='store_true', help='Overwrites existing file. Use with -f')
-    parser.add_argument('--debug',dest='debug', default=False, action='store_true' )
-    parser.add_argument('--debug_file', dest='debug_file', default='pparse.log', help='Filename of debuglog')
+    parser.add_argument('-O', '--overwrite', dest='overwrite', default=False, action='store_true',
+                        help='Overwrites existing file. Use with -f')
+    parser.add_argument('--debug', dest='debug', default=False, action='store_true')
+    parser.add_argument('--debug_file', dest='debug_file', help='Filename of debuglog')
 
+    args = parser.parse_args()
 
-    args = parser.parse_args(arguments)
+    ## SECTION - Logging
+    # Set logging level
+    if args.debug:
+        debug_level = logging.DEBUG
+    else:
+        debug_level = logging.INFO
+
+    if not args.debug_file:
+        debug_file = 'pparse.log'
+
+    # Create basic logging
+    logging.basicConfig(
+            level=debug_level,
+            filename=debug_file,
+            filemode='a',
+            format='%(asctime)s:%(levelname)s:%(message)s')
 
     # TODO perform functions on input
-    if in_file:
+    logging.info("""
+    ============================== START ==============================
+    """)
+    if args.in_file:
+        # Open File and read contents to list 'data'
+        try:
+            logging.info('Opening input file: {}'.format(args.in_file))
+            fh = open(args.in_file)
+            data_list = []
+            data_list = [l for l in fh.readlines() if l not in data_list]
+
+        except IOError as e:
+            logging.error('Error opening {}: {}'.format(args.in_file, e))
+            print('Error opening {}: {}'.format(args.in_file, e))
+        finally:
+            try:
+                fh.close()
+            except Exception as e:
+                logging.error('unable to close file')
+                print('Error: {}'.format(e))
+        # Open file to write out
+        try:
+            if args.overwrite:
+                fh = open(args.in_file, 'w')
+                filename = args.in_file
+            elif args.out_file:
+                fh = open(args.out_file, 'w')
+                filename = args.out_file
+            for line in data_list:
+                logging.debug('Filename set to {}'.format(filename))
+                findall_url_write(line, fh)
+                logging.info('Succcessfully wrote to {}'.format(filename))
+        except Exception as e:
+            logging.error('ERROR Writing File {}: {}'.format(filename, e))
+            print('Error: {}'.format(e))
+        finally:
+            fh.close()
+            logging.debug('Closing file')
+            logging.info("""
+            ============================== END ==============================
+            """)
+    elif args.in_blob:
         pass
-    elif in_blob:
+    elif args.in_url:
         pass
-    elif in_url:
-
-
-
-
-
 
 
 if __name__ == '__main__':
